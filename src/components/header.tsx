40615292bd9f8a3c4fc5e5744611284b82517e99
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-05-09 07:39:41 +0700

    home-page update contact

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -47,11 +47,11 @@ const Header: React.FC = () => {
                 Blog
               </Link>
               <Link
-                to="/travel-styles"
+                to="/contact"
                 onClick={closeMobileMenu}
                 className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
               >
-                Travel Styles
+                Contact
               </Link>
             </div>
           </div>
@@ -118,11 +118,11 @@ const Header: React.FC = () => {
               Blog
             </Link>
             <Link
-              to="/travel-styles"
+              to="/contact"
               onClick={closeMobileMenu}
               className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
             >
-              Travel Styles
+              Contact
             </Link>
           </div>
           <div className="pt-4 pb-3 border-t border-gray-200">
