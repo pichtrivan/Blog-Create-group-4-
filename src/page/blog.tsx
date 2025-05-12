f1fb71817ed9348d689624398a4fe60a84e4de17
Hongsophaline <hong.sophaline@institute.pse.ngo>
2025-05-12 15:18:54 +0700

    merge detail-blog page to main

--- src/page/blog.tsx
+++ src/page/blog.tsx
@@ -1,18 +1,5 @@
 const Blog = () => {
-<<<<<<< HEAD
-    return (
-      <div className="text-center p-8 mt-5">
-        <h1 className="text-4xl font-bold">Blog</h1>
-        <p className="mt-4">Read our latest posts.</p>
-      </div>
-    );
-  };
-  
-  export default Blog;
-  
-=======
   return <div>Blog</div>;
 };
 
 export default Blog;
->>>>>>> blog-detail
