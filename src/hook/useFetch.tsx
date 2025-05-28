d725a5e31a55c9c6a884d0eb6f66ea975488fce4
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-05-28 16:37:25 +0700

    main update usefetch

---
+++ src/hook/useFetch.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function useFetch<T = unknown>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(url);
+        setData(response.data.data); // Adjust depending on Strapi or other backend shape
+      } catch (err: any) {
+        setError(err.message || "Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
